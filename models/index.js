257da2377d784aa088322f48efc89b0d2869c3d2
Tushar Agrawal <tusharagrawal16@rediffmail.com>
2020-12-15 13:54:41 +0530

    Add the Yaml File for Deploy to DO

--- models/index.js
+++ models/index.js
@@ -1,9 +1,9 @@
 var mysql = require("mysql");
 var db_config = {
-  host: "opencssdatabase-do-user-8430368-0.b.db.ondigitalocean.com", // Host name   Eg. localhost
-  user: "doadmin", // Username in the database    Eg. root
+  host: process.env.DATABASE_URL, // Host name   Eg. localhost
+  user: process.env.DATABASE_USER, // Username in the database    Eg. root
   password: process.env.DATABASE_PASSWORD, // Password in the database Eg. password
-  database: "defaultdb", // Database You want to connect to   Eg. Flight Database
+  database: process.env.DATABASE_NAME, // Database You want to connect to   Eg. Flight Database
   port: 25060, // Port
   ssl: true, //
 };
